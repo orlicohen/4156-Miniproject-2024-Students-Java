fa889dbad1ad219970209b64b80f7d09f8be2062
Orli Cohen <oec2109@barnard.edu>
2024-09-13 13:15:39 -0400

    adding missed files

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -1,36 +1,46 @@
 package dev.coms4156.project.individualproject;
 
-import java.io.*;
+import java.io.Serial;
+import java.io.Serializable;
 
+/**
+ * Represents a class within a department. This class stores information about
+ * the course, including the instructor name, location, time slot, capacity, and 
+ * number of enrolled students. 
+ */
 public class Course implements Serializable {
 
   /**
    * Constructs a new Course object with the given parameters. Initial count starts at 0.
    *
-   * @param instructorName     The name of the instructor teaching the course.
-   * @param courseLocation     The location where the course is held.
-   * @param timeSlot           The time slot of the course.
-   * @param capacity           The maximum number of students that can enroll in the course.
+   * @param instructorName The name of the instructor teaching the course.
+   * @param courseLocation The location where the course is held.
+   * @param timeSlot The time slot of the course.
+   * @param capacity The maximum number of students that can enroll in the course.
    */
   public Course(String instructorName, String courseLocation, String timeSlot, int capacity) {
     this.courseLocation = courseLocation;
     this.instructorName = instructorName;
     this.courseTimeSlot = timeSlot;
     this.enrollmentCapacity = capacity;
-    this.enrolledStudentCount = 500;
+    this.enrolledStudentCount = 0;
   }
 
- /**
+  /**
    * Enrolls a student in the course if there is space available.
    *
    * @return true if the student is successfully enrolled, false otherwise.
    */
   public boolean enrollStudent() {
-   enrolledStudentCount++;
+    if (!isCourseFull()) {
+      enrolledStudentCount++;
+      return true;
+    }
+
     return false;
   }
 
- /**
+  /**
    * Drops a student from the course if a student is enrolled.
    *
    * @return true if the student is successfully dropped, false otherwise.
@@ -40,49 +50,82 @@ public class Course implements Serializable {
     return false;
   }
 
-
+  /**
+   * Gets course location. 
+   *
+   * @return the location of the course. 
+   */
   public String getCourseLocation() {
-    return this.instructorName;
-  }
-
-
-  public String getInstructorName() {
     return this.courseLocation;
   }
 
+  /**
+   * Gets instructor name for a course. 
+   *
+   * @return the instructor name.
+   */
+  public String getInstructorName() {
+    return this.instructorName;
+  }
 
+  /**
+   * Gets time slot for a course.
+   *
+   * @return the time slot.  
+   */
   public String getCourseTimeSlot() {
     return this.courseTimeSlot;
   }
 
-
-  public String toString() {
-    return "\nInstructor: " + instructorName +  "; Location: "  + courseLocation +  "; Time: " + courseTimeSlot;
+  /**
+   * Gets enrollment capacity for course.
+   *
+   * @return the enrollment capacity.  
+   */
+  public int getEnrollmentCapacity() {
+    return this.enrollmentCapacity;
   }
 
+  public String toString() {
+    return "\nInstructor: " + instructorName + "; Location: " + courseLocation + "; Time: "
+        + courseTimeSlot;
+  }
 
+  /**
+  * Reassigns course's instructor. 
+  */
   public void reassignInstructor(String newInstructorName) {
     this.instructorName = newInstructorName;
   }
 
-
+  /**
+  * Reassign courses's location. 
+  */
   public void reassignLocation(String newLocation) {
     this.courseLocation = newLocation;
   }
 
-
+  /**
+  * Reassigns course's time slot.
+  */
   public void reassignTime(String newTime) {
     this.courseTimeSlot = newTime;
   }
 
-
+  /**
+  * Sets course's enrollment count. 
+  */
   public void setEnrolledStudentCount(int count) {
     this.enrolledStudentCount = count;
   }
 
-
+  /**
+   * Checks if a course is full. 
+   *
+   * @return true if the course is full, false otherwise.
+   */
   public boolean isCourseFull() {
-    return enrollmentCapacity > enrolledStudentCount;
+    return enrollmentCapacity <= enrolledStudentCount;
   }
 
   @Serial
--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,25 +1,132 @@
 package dev.coms4156.project.individualproject;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Unit tests for Course class.
+ */
 @SpringBootTest
 @ContextConfiguration
 public class CourseUnitTests {
 
+  /**
+   * Setup Course instance for testing.
+   */
   @BeforeAll
   public static void setupCourseForTesting() {
     testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
   }
 
 
+  /**
+   * Test Course class toString() method.
+   */
   @Test
   public void toStringTest() {
+    // reassign all values so we know what expected values for course are
+    testCourse.reassignInstructor("Griffin Newbold");
+    testCourse.reassignLocation("417 IAB");
+    testCourse.reassignTime("11:40-12:55");
     String expectedResult = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
     assertEquals(expectedResult, testCourse.toString());
+  }
+
+  /**
+   * Test Course class enrollStudent() method.
+   */
+  @Test
+  public void testEnrollStudent() {
+    boolean expectedResult = !testCourse.isCourseFull();
+    assertEquals(expectedResult, testCourse.enrollStudent());
+  }
+
+  /**
+   * Test Course class getCourseLocation() method.
+   */
+  @Test
+  public void testGetCourseLocation() {
+    String expectedResult = "417 IAB";
+    // reassign value so we know what expected value for course is
+    testCourse.reassignLocation("417 IAB");
+    assertEquals(expectedResult, testCourse.getCourseLocation());
+  }
+
+  /**
+   * Test Course class getInstructorName() method.
+   */
+  @Test
+  public void testGetInstructorName() {
+    String expectedResult = "Griffin Newbold";
+    // reassign value so we know what expected value for course is
+    testCourse.reassignInstructor("Griffin Newbold");
+    assertEquals(expectedResult, testCourse.getInstructorName());
+  }
+
+  /**
+   * Test Course class getCourseTimeSlot() method.
+   */
+  @Test
+  public void testGetCourseTimeSlot() {
+    String expectedResult = "11:40-12:55";
+    // reassign value so we know what expected value for course is
+    testCourse.reassignTime("11:40-12:55");
+    assertEquals(expectedResult, testCourse.getCourseTimeSlot());
+  }
+
+  /**
+   * Test Course class reassignInstructor() method.
+   */
+  @Test
+  public void testReassignInstructor() {
+    String expectedResult = "Adam Cannon";
+    testCourse.reassignInstructor("Adam Cannon");
+    assertEquals(expectedResult, testCourse.getInstructorName());
+  }
+
+  /**
+   * Test Course class reassignLocation() method.
+   */
+  @Test
+  public void testReassignLocation() {
+    String expectedResult = "309 HAV";
+    testCourse.reassignLocation("309 HAV");
+    assertEquals(expectedResult, testCourse.getCourseLocation());
+  }
+
+  /**
+   * Test Course class reassignTime() method.
+   */
+  @Test
+  public void testReassignTime() {
+    String expectedResult = "4:10-5:25";
+    testCourse.reassignTime("4:10-5:25");
+    assertEquals(expectedResult, testCourse.getCourseTimeSlot());
+  }
+
+  /**
+   * Test Course class isCourseFull() method, when course enrollment is set to full.
+   */
+  @Test
+  public void testIsCourseFullTrue() {
+    testCourse.getEnrollmentCapacity();
+    testCourse.setEnrolledStudentCount(250);
+    assertTrue(testCourse.isCourseFull());
+  }
+
+  /**
+   * Test Course class isCourseFull() method, when course enrollment is set to the max.
+   */
+  @Test
+  public void testIsCourseFullFalse() {
+    testCourse.setEnrolledStudentCount(200);
+    assertFalse(testCourse.isCourseFull());
   }
 
   /** The test course instance used for testing. */
